6dabf35e73b8e2f37a4d720a71aa0d6137d6acf4
Sandeep Shukla <sandeepini.2012@gmail.com>
2021-06-22 18:41:58 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/np-task11-userManagement'));
+app.use(express.static(__dirname + '/dist/np-user-management'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/np-task11-userManagement/index.html'));
+res.sendFile(path.join(__dirname+'/dist/np-user-management/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
